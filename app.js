a4801132aa89938e06afb66c3bf57e90074f1293
MarieSerroyen <marie.serroyen@gmail.com>
2023-03-18 13:48:27 +0100

    feat: add local  mongoose connection

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var createError = require('http-errors');
 var express = require('express');
+const mongoose = require('mongoose');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require ('cors');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+mongoose.connect('mongodb://localhost:27017/fshn-backend-admin', {useNewUrlParser: true, useUnifiedTopology: true})
 var app = express();
 
 // view engine setup
