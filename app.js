0592046ee7f3b4360cf54acb27328e2b2dc58ca6
MarieSerroyen <marie.serroyen@gmail.com>
2023-03-18 14:29:21 +0100

    feat: save user to database

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require ('cors');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/api/v1/users');
 
 mongoose.connect('mongodb://localhost:27017/fshn-backend-admin', {useNewUrlParser: true, useUnifiedTopology: true})
 var app = express();
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,25 +3,24 @@ const User = require('../models/User');
 
 //POST create company user
 const create = async (req, res) => {
-    const { name, email, password } = req.body;
-    
-    const user = new User({ name, email, password });
+    try {
+        const { name, email, password } = req.body;
 
-    user.save ( (err, user) => {
-        if (err) {
-            return res.status(400).json({
-                status: 'error',
-                message: 'Something went wrong. User not created',
-                error: err
+        const user = new User({ name, email, password });
+
+        user.save()
+            .then(user => {
+                return res.status(200).json({status: "success", message: "User created successfully.", data: user });
+            })
+            .catch(err => {
+            return res.status(400).send({status: "failed", message: "Something went wrong, user not created", error: err });
             });
-        } else {
-            return res.status(200).json({
-                status: 'success',
-                message: 'User created successfully',
-                data: user
-            });
-        };
-    });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({status: "failed", message: "Something went wrong, user not created", error: error });
+    }
 };
+
 
 module.exports.create = create;
