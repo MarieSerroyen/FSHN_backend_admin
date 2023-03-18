ef5e91884519785ddc08d74490416417e69d1bab
MarieSerroyen <marie.serroyen@gmail.com>
2023-03-18 14:55:53 +0100

    feat: GET all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,33 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+//GET all users
+const getAll = async (req, res) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({status: "success", message: "Users fetched successfully.", data: users });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({status: "failed", message: "Something went wrong, users not fetched", error: error });
+    }
+};
 
 //POST create company user
 const create = async (req, res) => {
     try {
         const { name, email, password } = req.body;
 
+        //Check if fields are empty
+        if(!name || !email || !password) {
+            return res.status(404).send({status: "failed", message: "Please fill all fields"});
+        }
+
         const user = new User({ name, email, password });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
 
         user.save()
             .then(user => {
@@ -22,5 +43,4 @@ const create = async (req, res) => {
     }
 };
 
-
-module.exports.create = create;
+module.exports = { getAll, create };
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('../../../controllers/userController');
 
 router.post('/', userController.create);
+router.get('/', userController.getAll);
 
 module.exports = router;
