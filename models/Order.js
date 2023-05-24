9d209a9c32fc990877757357934d12c848c4b3de
MarieSerroyen <marie.serroyen@gmail.com>
2023-05-24 07:52:22 +0200

    feat: make order model

---
+++ models/Order.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    storeId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Store',
+        required: true 
+    },
+    productIds: {
+        type: Array,
+        required: true
+    },
+    orderNumber: {
+        type: Number,
+        required: true
+    },
+    clientNumber:{
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: false
+    }
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
